72a4fd8a45c675f79203ced7e490782df6180790
just-atem <dorrchep@gmail.com>
2020-01-20 16:49:34 +0300

    updated database

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
 import React from "react";
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 
-import Navbar from "./components/client/Navbar/Navbar";
-
-import About from "./components/client/About/About";
-import Contact from "./components/client/Contact/Contact";
-import Login from "./components/client/Login/Login";
-import Signup from "./components/client/Signup/Signup";
-import AddProduct from "./components/client/Products/AddProduct";
+import Navbar from "./components/client/navbar";
+import home from "./components/client/home";
+import About from "./components/client/about";
+import Login from "./components/client/login";
+import Signup from "./components/client/signup";
+import Mobilephones from "./components/client/mobilephones";
+import Clothes from "./components/client/clothes";
+import Laptops from "./components/client/laptops";
+import Contact from "./components/client/Contact";
 import Products from "./components/client/Products/Products";
-import LandingPage from "./components/client/Main-Page/LandingPage";
-import Clothes from "./components/client/Products/Clothes";
-import Checkout from "./components/client/Products/Checkout";
-import Laptops from "./components/client/Products/Laptops";
-import Phones from "./components/client/Products/Phones";
-import Footer from "./components/client/Footer/Footer";
-
+import AddProduct from "./components/client/Products/AddProduct";
+import Checkout from "./components/client/checkout/Checkout";
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      
+      <Navbar />
       <Router>
         <Switch>
-        <Route exact path="/" component={LandingPage}/>
-        <Route path="/signup" component={Signup}/>
-        <Route path="/login" component={Login}/>      
-         <Route path="/about" component={About}/>
-         <Route path="/contact" component={Contact}/>
-         <Route path="/add" component={AddProduct}/>
-         <Route path="/products" component={Products}/>   
-         <Route path="/clothes" component={Clothes}/>
-         <Route path="/checkout" component={Checkout}/>
-         <Route path="/laptops" component={Laptops}/>
-         <Route path="/phones" component={Phones}/>
-         
+          <Route path="/" exact component={home} />
+          <Route path="/about" component={About} />
+          <Route path="/products" component={Products} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/mobilephones" component={Mobilephones} />
+          <Route path="/clothes" component={Clothes} />
+          <Route path="/laptops" component={Laptops} />
+          <Route path="/checkout/:id" component={Checkout} />
+
+          <Route path="/contact" component={Contact} />
+          <Route path="/add-product" component={AddProduct} />
         </Switch>
       </Router>
     </div>
